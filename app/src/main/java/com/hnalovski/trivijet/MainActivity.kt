c1ab1b4aa066e6c0a590a9a49c9d49f05d39ae43
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-03-17 01:09:09 +0100

    Initial commit

---
+++ app/src/main/java/com/hnalovski/trivijet/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hnalovski.trivijet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hnalovski.trivijet.ui.theme.TrivijetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TrivijetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TrivijetTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/hnalovski/trivijet/model/Question.kt
@@ -0,0 +1,3 @@
+package com.hnalovski.trivijet.model
+
+class Question : ArrayList<QuestionItem>()
