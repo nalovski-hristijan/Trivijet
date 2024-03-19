234a2f3af85ab4568b9163911b29d36d1abf01b6
Hristijan Nalovski <nalovski.hristijan@protonmail.com>
2024-03-19 17:04:43 +0100

    Implemented the ViewModel

--- app/src/main/java/com/hnalovski/trivijet/MainActivity.kt
+++ app/src/main/java/com/hnalovski/trivijet/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hnalovski.trivijet
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.hnalovski.trivijet.screens.QuestionsViewModel
 import com.hnalovski.trivijet.ui.theme.TrivijetTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.log
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+    Questions(viewModel)
+}
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions = viewModel.data.value.data?.toMutableList()
+    if (viewModel.data.value.loading == true) {
+        Log.d("LOADING", "Questions: loading ...")
+    } else {
+        Log.d("LOADING", "Questions: loading stopped ")
+        questions?.forEach{questionItem ->
+            Log.d("RESULT", "Questions: ${questionItem.question}")
+        }
+    }
+    Log.d("SIZE", "Questions: ${questions?.size}")
+
 }
 
 @Preview(showBackground = true)
